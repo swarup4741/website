312a605507c86939786a7a9fa9dd5f3a3408a914
Swarup Kumar Das <313swarup@gmail.com>
2021-09-10 03:19:38 +0530

    re-commit

--- components/blog/MDXComponents.tsx
+++ components/blog/MDXComponents.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import InlineCode from './InlineCode'
 
 export const MDXComponents = {
-  // eslint-disable-next-line react/display-name
   Image,
   // eslint-disable-next-line react/display-name
   a: (props: any) => (
