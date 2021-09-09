8c196d741d9cba31f1e085e95169dc1c1026a028
Swarup Kumar Das <69234404+swarup4741@users.noreply.github.com>
2021-09-10 03:32:26 +0530

    Update BlogPost.tsx

--- components/blog/BlogPost.tsx
+++ components/blog/BlogPost.tsx
@@ -33,6 +33,7 @@ export default function BlogPost({
             objectFit="cover"
             className="rounded-full"
             alt="blog author avatar"
+            priority
           />
           <div className="flex separator">
             <p className="mr-2 text-gray-900 meta-size dark:text-gray-100">
@@ -62,6 +63,7 @@ export default function BlogPost({
         className="rounded-sm"
         objectFit="cover"
         alt="blog image"
+        priority
       />
     </>
   )
