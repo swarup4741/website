d7ac7602da305fda5ae23ef8804e2c7c2188e02e
Swarup Kumar Das <313swarup@gmail.com>
2021-09-15 01:54:06 +0530

    ui style changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,8 +7,11 @@ interface Props {
 
 export default function Button({ href, children }: Props) {
   return (
-    <button className="w-full px-10 py-2 text-sm text-white transition-all bg-black border rounded-md sm:text-base md:text-lg hover:border-gray-800 hover:bg-transparent hover:text-black dark:hover:border-gray-100 dark:text-black dark:bg-white dark:hover:bg-transparent dark:hover:text-white">
-      <a href={href}>{children}</a>
-    </button>
+    <a
+      href={href}
+      className="grid w-full px-10 py-2 text-sm text-white transition-all bg-black border rounded-md place-items-center sm:text-base md:text-lg hover:border-gray-800 hover:bg-transparent hover:text-black dark:hover:border-gray-100 dark:text-black dark:bg-white dark:hover:bg-transparent dark:hover:text-white"
+    >
+      {children}
+    </a>
   )
 }
